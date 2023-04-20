2e7c0c761653ccb27ffb6c37f481bc56aa0ce3bc
wojtek777 <49556059+wojtek777@users.noreply.github.com>
2023-04-21 00:06:58 +0200

    Add files via upload

---
+++ code/Configuration_module.py
@@ -0,0 +1,17 @@
+start_configuration_module ():
+	pass
+
+save_profile (current_profile):
+	pass
+	
+save_config_file():
+	pass
+	
+put_settings_front():
+	pass
+	
+put_profile_to_front (current_profile):
+	pass
+	
+read_config_file (name):
+	pass
