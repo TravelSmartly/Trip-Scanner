37d116d526306a4e2181c4fca1c898fc34531795
Nauteirius <kamil.sordyl@student.uj.edu.pl>
2023-04-27 11:03:31 +0200

    Update, new tests

--- code/location_module.py
+++ code/location_module.py
@@ -3,7 +3,7 @@ from kivy.uix.label import Label
 from plyer import gps
 import pytest
 
-class Localisation_module(App):
+class Location_module(App):
 
     current_location = []
     s_string:str
@@ -17,8 +17,7 @@ class Localisation_module(App):
 
     def start_location_module(*args):
     pass
-    def on_start(self):
-    pass
+
     def put_user_location_front(self):
 
 
--- code/test_location_module.py
+++ code/test_location_module.py
@@ -1,39 +1,36 @@
-from localisation_module import Localisation_module
+from location_module import Location_module
 import pytest
 
 
 @pytest.fixture
-def localisation_module():
-    return Localisation_module()
+def location_module():
+    return Location_module()
 
 
-def test_get_current_location(localisation_module):
+def test_get_current_location(location_module):
     # check if location return is not empty thing
-    assert localisation_module.get_current_location() is not None
+    assert location_module.get_current_location() is not None
 
 
-def test_check_proximity(localisation_module):
+def test_check_proximity(location_module):
     r=100
     current_location = (50.06163905474776, 19.936530642326513)
     real_pos = (50.06182501735149, 19.933510474939244)
-    assert localisation_module.Check_proximity(r,current_location,real_pos) = True #Check proximiy ((current_location[0]-real_pos[0])**2 + (current_location[1]-real_pos[1])**2) < r
+    assert location_module.Check_proximity(r,current_location,real_pos) = True #Check proximiy ((current_location[0]-real_pos[0])**2 + (current_location[1]-real_pos[1])**2) < r
 
     real_pos = (50.05301179901293, 19.914758618374478)
-    assert localisation_module.Check_proximity(r, current_location, real_pos) = False
+    assert location_module.Check_proximity(r, current_location, real_pos) = False
     #r, current_location, faktycznalokalizacji
 
     # set fixed location check whats nearbly
 
 
 
-def test_on_start(localisation_module):
-    #
-    assert localisation_module.on_start() == None
 
 
-def test_put_user_location_front(localisation_module):
+def test_put_user_location_front(location_module):
     #
-    assert localisation_module.put_user_location_front() == None
+    assert location_module.put_user_location_front() == None
 
 
 def test_start_location_module(monkeypatch, localisation_module):
