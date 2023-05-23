7e2d7509d9c83338238ea2061aabeb35b920eb57
wojtek777 <49556059+wojtek777@users.noreply.github.com>
2023-05-23 15:41:05 +0200

    changes to methods

--- code/back_end/location_module.py
+++ code/back_end/location_module.py
@@ -1,12 +1,8 @@
-from kivy.app import App
-from kivy.uix.label import Label
-from plyer import gps
-
 class Location_module:
     lat = 111045 #one degree of latitude is always 69 miles = 111045 meters
-	lon = 111045 #one degree of latitude in equator, sea level is 69 miles = 111045 meters
-	center_location: tuple[float, float] #last saved location of a user
-	current_location: tuple[float, float] 
+    lon = 111045 #one degree of latitude in equator, sea level is 69 miles = 111045 meters
+    center_location = (10, 10) #last saved location of a user
+    current_location = (10, 10)
     #s_string:str
     #type hints
 
@@ -15,39 +11,32 @@ class Location_module:
 
     @classmethod
     def on_gps_location(cls, *args, **kwargs)->None:
-        #kwargs["lat"]=10.0
-        #kwargs["lon"]=10.0
-        #print(kwargs)
         cls.current_location = tuple((kwargs["lat"], kwargs["lon"])) 
 
+    ### This method should return a tuple 
+    ### with current user coordinates
     @classmethod
-    def get_current_location(cls)->int:
-        try:
-            gps.configure(on_location=cls.on_gps_location)
-            gps.start()
-            time.sleep(1)
-            gps.stop()
-            #coordinates = gps.
-            return cls.current_location
-        except Exception as e:
-            return -1
+	def get_current_location(cls):
+		try:
+			return cls.current_location
+		except Exception as e:
+			return -1
 
+    ### This method should return true if current_location
+    ### is inside an elipse with the center in center_location variable
     @classmethod
     def check_proximity(cls,r,current_location,center_location)->bool:
-        #zwraca true jesli current_location jest w srodku elipsy o srodku w punkcie center_location
         return (cls.current_location[0]-cls.center_location[0] * cls.lat) ^ 2 + (cls.current_location[1] - cls.center_location[1] * cls.lon * math.cos((cls.current_location[0] + cls.center_location[0])/2))^2 < (r)^2
 
 
-
-
-
+    ### Method that allows to get user current location
+    ### to front
     @classmethod
     def put_user_location_front(cls):
         tmp = cls.get_current_location()
-        if tmp == -1:
+        if tmp == (-1, -1):
             print("gps not working")
         return tmp
-        #give coordinated for current or center location
 
     @classmethod
     def start_location_module(cls, r, center_location):
