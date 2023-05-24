aa05e754145c2af29f0c9e6d8a8da6a85666c5aa
RemXYZ <remkarablin20@gmail.com>
2023-05-24 21:09:30 +0200

    back-end, changes from RemXYZ

--- code/back_end/configuration_module.py
+++ code/back_end/configuration_module.py
@@ -1,5 +1,8 @@
+import pathlib
+import json
 
-@dataclass
+## RemXYZ: Dlaczego ta referencja?
+# @dataclass
 class Configuration_module:
 	profile_current: str = '' #blank profile default
 	profiles_object = '' #json object with profiles
@@ -9,49 +12,52 @@ class Configuration_module:
 	notification_system: bool = True 
 	night_mode: bool = False
 
-	def put_profiles_to_front ():
+	def hi(self):
+		print("HI IT IS Configuration_module")
+
+	def put_profiles_to_front (self):
 		return self.profile_object
 	
-	def put_profile_name_front ():
+	def put_profile_name_front (self):
 		return self.profile_current
 
-	def set_current_profile (current_profile_object):
+	def set_current_profile (self, current_profile_object):
 		self.profile_current = current_profile_object
 
-	def save_profiles ():
+	def save_profiles (self,):
 		config_folder_path = pathlib.Path('../config/profiles.json')
 		with open(config_folder_path, 'w') as fwd:
 			json.dump (self.profile_object, fwd)
 
-	def read_profiles ():
+	def read_profiles (self,):
 		config_folder_path = pathlib.Path('../config/profiles.json')
 		with open(config_folder_path) as frd:
 			self.profile_object = json.loads(frd.read())
 
-	def save_config_file(settings):
+	def save_config_file(self,settings):
 		config_folder_path = pathlib.Path('../config/config_file.cfg')
-    	with open("config_file.conf", 'w') as fwd:
+		with open("config_file.conf", 'w') as fwd:
 			fwd.write (self.first_timer + '\n' +
-			self.notification_system + '\n' + 
+			self.notification_system + '\n' +
 			self.interval + '\n' +
 			self.night_mode + '\n' +
 			self.profile_current + '\n')
 	#for first version: notification w
-	def read_config_file (name):
+	def read_config_file (self, name):
 		config_folder_path = pathlib.Path('../config/config_file.cfg')
-    	with open("config_file.conf") as frd:
+		with open("config_file.conf") as frd:
 			conf = list(frd.read().split('\n'))
-			self.notification_system, self.interval, self.night_mode, self.profile_current = *conf
+			self.notification_system, self.interval, self.night_mode, self.profile_current = conf
 			
 
-	def put_settings_front():
+	def put_settings_front(self):
 		pass
 
 	
 
 	
 
-	def start_configuration_module():
+	def start_configuration_module(self):
 		pass
 		#Does not return any specific objects, so just make sure that it returns 0 if succeeded
 		#Returns -7 if config_file does not exist or could not have been opened
