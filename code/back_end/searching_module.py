2f2170d2b43fe17ed821feef54d17706dc35f4c9
Nauteirius <97364999+Nauteirius@users.noreply.github.com>
2023-05-25 18:27:12 +0200

    Update searching_module.py

--- code/back_end/searching_module.py
+++ code/back_end/searching_module.py
@@ -23,3 +23,29 @@ class Searching_module:
             return -1
         else:
             return 0
+        
+        
+#test with request, worth to try gmaps won't work well
+import requests
+
+# set up the API endpoint and parameters
+url = "https://maps.googleapis.com/maps/api/place/radarsearch/json"
+params = {
+    "location": "50.060683036581125,19.935779508513296", # latitude,longitude of the center of the search area
+    "radius": 5000, # search radius in meters
+    "type": "restaurant", # type of place to search for
+    "key": "" # replace with your own API key
+}
+
+# send the request and get the response
+response = requests.get(url, params=params)
+print(response)
+#data = response.json()
+
+# extract the place_ids
+# place_ids = [result["place_id"] for result in data["results"]]
+# while "next_page_token" in data:
+#     params["pagetoken"] = data["next_page_token"]
+#     response = requests.get(url, params=params)
+#     data = response.json()
+#     place_ids += [result["place_id"] for result in data["results"]]
