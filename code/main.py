f7cd0d9503f78b3c2f78a30ad7a121e3273fb025
wojtek777 <49556059+wojtek777@users.noreply.github.com>
2023-05-25 19:34:19 +0200

    Added gps system to main branch

--- code/main.py
+++ code/main.py
@@ -4,6 +4,7 @@ import json
 import pathlib
 from time import time
 from kivy.app import App
+from kivy.utils import platform
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
@@ -44,7 +45,18 @@ class MainApp:
 
     def start(self):
         ###-- BACK-END PART --###
+
+        ### GPS system -> may not work on devices other than android!
+        ### starts updating current phone location
+        if platform == 'android':
+            from android.permissions import request_permissions, Permission
+            request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE, Permission.INTERNET, Permission.ACCESS_FINE_LOCATION, Permission.ACCESS_COARSE_LOCATION])
+            gps.configure(on_location=Location_module.on_gps_location)
+            gps.start(minTime=1000, minDistance=0)
+        ###########################################################
+
         config_module = Configuration_module()
+        config_module.create_config_file()
         profile_import_status = config_module.read_profiles()
         categories_import_status = config_module.read_categories()
         # print(categories_import_status, profile_import_status)
