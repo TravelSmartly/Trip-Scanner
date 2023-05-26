c8398d881e6b3958d3256ff99124a79b785004a1
wojtek777 <49556059+wojtek777@users.noreply.github.com>
2023-05-26 22:39:47 +0200

    searching_module_basics

--- code/back_end/searching_module.py
+++ code/back_end/searching_module.py
@@ -1,21 +1,33 @@
+import googlemaps
+
 class Searching_module:
     #search_counter = 0
     # init method or constructor
+    API_KEY = 'insert_api_key_here'
+
     def __init__(self, current_location = None):
         self.m_object_list = []
         if current_location:
             self.m_object_list[0] = current_location
-
-
+    
+    def convert_miles_to_meters (miles):
+        try:
+            return miles * 1_609.344
+        except:
+            return -1
 
     def get_m_object_list(self):
         return self.m_object_list
+        
     def set_m_object_list(self,list):
         self.m_object_list = list
+
     def search_the_area(self):
         pass
+
     def remove_old_objects(self):
-        pass
+        self.m_object_list = []
+
     def start_searching_module(self, current_location):
         if (current_location == ()):
             return -2
@@ -23,23 +35,32 @@ class Searching_module:
             return -1
         else:
             return 0
+
+    def start_searching_module(self, input_location = None, input_radius = None):
+        map_client = googlemaps.Client(API_KEY)
+        if (input_location):
+            response = map_client.places_nearby (location = input_location, radius = input_radius)
+
+    def prepare_search_result (self, params):
+        pass
+
         
         
 #test with request, worth to try gmaps won't work well
-import requests
+#import requests
 
 # set up the API endpoint and parameters
-url = "https://maps.googleapis.com/maps/api/place/radarsearch/json"
-params = {
-    "location": "50.060683036581125,19.935779508513296", # latitude,longitude of the center of the search area
-    "radius": 5000, # search radius in meters
-    "type": "restaurant", # type of place to search for
-    "key": "" # replace with your own API key
-}
+#url = "https://maps.googleapis.com/maps/api/place/radarsearch/json"
+#params = {
+    #"location": "50.060683036581125,19.935779508513296", # latitude,longitude of the center of the search area
+    #"radius": 5000, # search radius in meters
+    #"type": "restaurant", # type of place to search for
+    #"key": "" # replace with your own API key
+#}
 
 # send the request and get the response
-response = requests.get(url, params=params)
-print(response)
+#response = requests.get(url, params=params)
+#print(response)
 #data = response.json()
 
 # extract the place_ids
